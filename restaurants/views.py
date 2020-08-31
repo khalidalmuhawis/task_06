322d92c5ecc0e1da6b284e945f4ecf61cb4ce50a
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-08-31 13:49:20 +0300

    task 06 create view

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Restaurant
+from .forms import RestaurantForm
 
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
@@ -18,8 +19,14 @@ def restaurant_detail(request, restaurant_id):
     return render(request, 'detail.html', context)
 
 def restaurant_create(request):
+    create_form = RestaurantForm()
+    if request.method == "POST":
+        create_form = RestaurantForm(request.POST)
+        if create_form.is_valid():
+            create_form.save()
+            return redirect('restaurants/list/')
 
     context = {
-
+    "form": create_form
     }
     return render(request, 'create.html', context)
